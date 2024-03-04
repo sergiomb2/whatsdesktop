38866da68587ad3e0e924600423b611715218196
Sérgio M. Basto <sergio@serjux.com>
2024-03-04 01:27:54 +0000

    run

--- runme.sh
+++ runme.sh
@@ -3,7 +3,14 @@ yarn upgrade
 yarn outdated
 ll node_modules/
 yarn start
-yarn build
+#yarn build
 #node_modules/.bin/electron-builder -l rpm
+yarn build --linux --dir
 
 
+_electron_dist=/usr/lib64/electron
+_electron_ver=$(cat ${_electron_dist}/version)
+npm exec -c "electron-builder --linux --dir -c.electronDist=${_electron_dist} -c.electronVersion=${_electron_ver} "
+
+
+electron dist/linux-unpacked/resources/app.asar &
